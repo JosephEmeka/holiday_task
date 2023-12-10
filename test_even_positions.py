0d9d598392ced97f0d7a2d03433cd5bb2c445bc3
Joseph <josephemekaonah@gmail.com>
2023-12-10 01:18:59 +0100

    test_even_positions.py

---
+++ test_even_positions.py
@@ -0,0 +1,7 @@
+import even_positions_func
+
+my_new_list = [9,11,5,-5,14,22,7,8]
+this_second_list = [-100, -1, -5, -9, -6, -8, -17]
+
+def test_odd_positions();
+	assert odd_positions.odd_position_func(this_List) == [9, 5, 14, 7]
