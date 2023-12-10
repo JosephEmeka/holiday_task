682eb1dfd9fe7b7ef4c08ed6c49b485725878ac1
Joseph <josephemekaonah@gmail.com>
2023-12-10 22:01:23 +0100

    test_odd_positions.py

--- test_odd_positions.py
+++ test_odd_positions.py
@@ -1,7 +1,7 @@
-import odd_positions_func
+import odd_positions
 
 my_new_list = [9,11,5,-5,14,22,7,8]
 this_second_list = [-100, -1, -5, -9, -6, -8, -17]
 
-def test_odd_positions_func();
-	assert odd_positions.odd_position_func(my_new_list) == [11, -5, 22, 8]
+def test_odd_positions_func():
+	assert odd_positions.odd_positions_func(my_new_list) == [11, -5, 22, 8]
