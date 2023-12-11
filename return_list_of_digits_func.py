732b6b563302dff8a04b0eacdb3ae44005793bdc
Joseph <josephemekaonah@gmail.com>
2023-12-11 11:58:05 +0100

    return_list_of_digits_func.py

---
+++ return_list_of_digits_func.py
@@ -0,0 +1,10 @@
+def return_list_of_digits_func(myNumber):
+  my_number = str(myNumber)
+  my_number_list = list(my_number)
+  return my_number_list
+  
+
+this_number = 234567
+print(return_list_of_digits_func(this_number))
+
+
