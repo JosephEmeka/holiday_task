8bcbfa40abdfa675442aa450041e96cc17af32a7
Joseph <josephemekaonah@gmail.com>
2023-12-11 01:26:35 +0100

    check_element_func.py

---
+++ check_element_func.py
@@ -0,0 +1,7 @@
+def check_element_func(myList, element):
+      if element in myList:
+        return True
+      else:
+        return False
+this_list = [9,11,5,-5,14,22,7,8]
+print(check_element_func(this_list, 20))
