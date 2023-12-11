1e3c3b9caa5c1f56565638dcc1c28e2da55d62de
Joseph <josephemekaonah@gmail.com>
2023-12-11 11:53:57 +0100

    test_return_list_of_digits_func.py

---
+++ test_return_list_of_digits_func.py
@@ -0,0 +1,6 @@
+import return_list_of_digits_func
+
+
+this_my_number = 98473567
+def test_return_list_of_digit_func():
+	assert return_list_of_digits_func.return_list_of_digits_func(this_my_number) == ["9", "8", "4", "7", "3", "5", "6", "7"]
