14beb724e7456559df76e65e4dc47b1c5043becd
Joseph <josephemekaonah@gmail.com>
2023-12-19 19:47:14 +0100

    test_check_duplicates.py

--- test_check_duplicates.py
+++ test_check_duplicates.py
@@ -3,9 +3,9 @@ import check_duplicates
 
 def test_check_duplicates():
 
-	fruits =['apple', 'orange', 'banana', 'apple', 'apple', 'grape']
+	fruits =['apple', 'orange', 'banana', 'apple', 'apple', 'grape', 'orange']
 
-	assert check_duplicates.check_duplicates(fruits) == ['apple']
+	assert check_duplicates.check_duplicates(fruits) == ['apple', 'orange']
 
 	names =['John', 'Tolu', 'Ada', 'Ada', 'Dayo', 'Kunle']
 
